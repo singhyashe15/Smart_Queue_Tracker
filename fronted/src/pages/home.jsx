e2eccc7f04993de4334402c2d030434605f4ca97
Yashraj Singh <130179859+singhyashe15@users.noreply.github.com>
2025-04-25 01:04:45 +0530

    Update home.jsx

--- fronted/src/pages/home.jsx
+++ fronted/src/pages/home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { HStack,Text, Link, Flex, Drawer, DrawerOverlay, DrawerCloseButton, DrawerBody, DrawerHeader, DrawerContent, IconButton, useDisclosure, VStack, Avatar } from "@chakra-ui/react";
+import { HStack, Text, Link, Flex, Drawer, DrawerOverlay, DrawerCloseButton, DrawerBody, DrawerHeader, DrawerContent, IconButton, useDisclosure, VStack, Avatar } from "@chakra-ui/react";
 import HomeComponent from "../components/homeComponent.jsx";
 import UserComponent from "../components/user/userComponent.jsx";
 import AdminComponent from "../components/admin/adminComponent.jsx";
@@ -31,15 +31,15 @@ export default function Home() {
 
         {/* Navigation Links */}
         {user === null && <HStack spacing={5} display={{ base: "none", md: "flex" }}>
-          <Link variant="plain" href="/admin" _hover={{ color: 'white',textDecoration: "none" }} color="white">Admin</Link>
-          <Link variant="plain" href="/register" _hover={{ color: 'white',textDecoration: "none" }} color="white">Register</Link>
-          <Link variant="plain" href="/login" _hover={{ color: 'white',textDecoration: "none" }} color="white">Login</Link>
+          <Link variant="plain" href="/admin" _hover={{ color: 'white', textDecoration: "none" }} color="white">Admin</Link>
+          <Link variant="plain" href="/register" _hover={{ color: 'white', textDecoration: "none" }} color="white">Register</Link>
+          <Link variant="plain" href="/login" _hover={{ color: 'white', textDecoration: "none" }} color="white">Login</Link>
         </HStack>
         }
         {
           user?.id > 0 &&
           <HStack display={{ base: "none", md: "flex" }}>
-            <Link variant="plain" href="/logout" _hover={{ color: 'white',textDecoration: "none" }} color="white">Logout</Link>
+            <Link variant="plain" href="/logout" _hover={{ color: 'white', textDecoration: "none" }} color="white">Logout</Link>
           </HStack>
         }
 
@@ -60,22 +60,28 @@ export default function Home() {
           {user !== null && <DrawerHeader>Hey,{user?.name}</DrawerHeader>}
           <DrawerBody>
             {user !== null &&
-              <Link
-                display="block"
-                mb={2}
-                href="/logout"
-                _hover={{ textDecoration: "none", color: "red.500" }}
-              >
-                <HStack spacing={2}>
-                  <BiLogOut size={20} />
-                  <Text>Logout</Text>
-                </HStack>
-              </Link>}
+              <VStack>
+                <Link
+                  display="block"
+                  mb={2}
+                  href="/logout"
+                  _hover={{ textDecoration: "none", color: "red.500" }}
+                >
+                  <HStack spacing={2}>
+                    <BiLogOut size={20} />
+                    <Text>Logout</Text>
+                  </HStack>
+                </Link>
+
+                <Link variant="plain" href="/dashboard" _hover={{ textDecoration: "none" }}>
+                  Dashboard
+                </Link>
+              </VStack>}
             {user === null &&
               <VStack align="left" spacing="4" pt="8">
-                <Link variant="plain" href="/admin" _hover={{textDecoration: "none"}} >Admin</Link>
-                <Link variant="plain" href="/register" _hover={{textDecoration: "none"}} >Register</Link>
-                <Link variant="plain" href="/login" _hover={{textDecoration: "none"}} >Login</Link>
+                <Link variant="plain" href="/admin" _hover={{ textDecoration: "none" }} >Admin</Link>
+                <Link variant="plain" href="/register" _hover={{ textDecoration: "none" }} >Register</Link>
+                <Link variant="plain" href="/login" _hover={{ textDecoration: "none" }} >Login</Link>
               </VStack>
             }
           </DrawerBody>
