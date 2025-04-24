12b5f0f5a3e469db58ce589d188714b45554dcba
Yashraj Singh <130179859+singhyashe15@users.noreply.github.com>
2025-04-25 00:49:57 +0530

    added

--- backend/routes/userroute.js
+++ backend/routes/userroute.js
@@ -5,7 +5,7 @@ import Appointment from "../controllers/user/appointment.js";
 import liveStatus from "../controllers/user/liveStatus.js";
 import deleteAppointment from "../controllers/user/deleteAppointment.js";
 import Chat from "../controllers/user/chat.js";
- // Ensure the file extension
+import TotalAppointment from "../controllers/user/totalAppointment.js";
 
 const router = express.Router();
 
@@ -16,10 +16,12 @@ router.post("/login",login);
 // for taking appointment
 router.post("/appointment",Appointment);
 // for checking live update of queue
-router.get('/livestatus/:id',liveStatus);
+router.get('/livestatus/:orgs/:id',liveStatus);
 // for deleting appointment
 router.delete('/deleteAppointment',deleteAppointment)
 // for chat-bot 
 router.post('/chat',Chat);
+// to get totalappointment of users
+router.get('/totalAppointment/:id',TotalAppointment)
 
 export default router;
