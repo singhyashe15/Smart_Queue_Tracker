c223c590171bb05707ed8c4c90e2d3ae5e10b668
Yashraj Singh <130179859+singhyashe15@users.noreply.github.com>
2025-04-25 00:48:01 +0530

    Update chat.js

--- backend/controllers/user/chat.js
+++ backend/controllers/user/chat.js
@@ -1,26 +1,13 @@
-import axios from "axios";
-
-const customReplies = [
-  { pattern: /how to login/i, reply: "To log in: go to the Login page at the top right , enter your email & password, then click Submit." },
-  { pattern: /book.*appointment/i, reply: "Go to the booking section, choose a organisation,enter patient name,email,date,department then confirm your slot." },
-  { pattern: /cancel.*appointment/i, reply: "Visit your dashboard, find the appointment, and click 'Cancel'." },
-  { pattern: /how long.*wait/i, reply: "You can view your estimated wait time on the live queue dashboard" },
-  { pattern: /queue status/i, reply: "Go to the 'My Queue' section to see your current position and estimated wait time." },
-
-  // add more patterns here…
-];
+import { customReplies } from "../../data/data.js";
 
 const chat = async (req, res) => {
+  console.log(req.body)
   const { prompt } = req.body;
-  console.log(prompt)
+  
   try {
     if (!prompt) {
-      return res.status(402).json({ reply: "Fill the query first" })
+      return res.status(403).json({ reply: "Fill the query first" })
     }
-    const headers = {
-      'Authorization': `Bearer ${process.env.API_KEY}`,
-      'Content-Type': 'application/json'
-    };
 
     for (let { pattern, reply } of customReplies) {
       if (pattern.test(prompt)) {
@@ -28,15 +15,7 @@ const chat = async (req, res) => {
       }
     }
 
-    const response = await axios.post(
-      'https://api-inference.huggingface.co/models/microsoft/DialoGPT-medium',
-      {
-        inputs: prompt
-      },
-      { headers }
-    );
-    console.log(response.data)
-    const reply = response.data[0].generated_text || "Sorry, I didn't get that.";
+    const reply = "Sorry, I didn't get this.";
     res.status(200).json({ reply });
   } catch (err) {
     console.error(err);
