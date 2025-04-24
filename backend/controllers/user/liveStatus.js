64076a1311bba4562a966cda16f1b1e0869be0aa
Yashraj Singh <130179859+singhyashe15@users.noreply.github.com>
2025-04-25 00:48:28 +0530

    final commit

--- backend/controllers/user/liveStatus.js
+++ backend/controllers/user/liveStatus.js
@@ -2,10 +2,10 @@ import pool from "../../config/db.js";
 
 const liveStatus = async(req,res)=>{
   try {
-    const {id} = req.params;
-    console.log(id)
-    const data = await pool.query("SELECT * from appointment WHERE userid=$1",[id]);
-    console.log(data)
+    const {orgs,id} = req.params;
+    console.log(orgs)
+    const data = await pool.query("SELECT * from appointment WHERE userid=$1 and organisation=$2",[id,orgs]);
+    
     if(data.rowCount == 0){
       return res.status(401).json({msg:"Not data Found",success:false});
     }
