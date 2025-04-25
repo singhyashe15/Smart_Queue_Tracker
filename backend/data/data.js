ccb8ac2ee06de5a88a959da437348ca361928b6e
Yashraj Singh <130179859+singhyashe15@users.noreply.github.com>
2025-04-25 15:10:58 +0530

    data added

---
+++ backend/data/data.js
@@ -0,0 +1,7 @@
+export const customReplies = [
+  { pattern: /how to login/i, reply: "To log in: go to the Login page at the top right , enter your email & password, then click Submit." },
+  { pattern: /book.*appointment/i, reply: "Go to the booking section, choose a organisation,enter patient name,email,date,department then confirm your slot." },
+  { pattern: /cancel.*appointment/i, reply: "Visit your dashboard, find the appointment, and click 'Cancel'." },
+  { pattern: /how long.*wait/i, reply: "You can view your estimated wait time on the live queue dashboard" },
+  { pattern: /queue status/i, reply: "Go to the 'My Queue' section to see your current position and estimated wait time." },
+];
